898ffc1128802a8aa52e5912ff0a1793016beb56
Adrian Roth <adrianroth10@gmail.com>
2016-03-27 13:19:45 +0200

    Method all is working
    tests for done and undone is also written

--- include/reader.h
+++ include/reader.h
@@ -1,21 +1,32 @@
 #ifndef READER_H
 #define READER_H
 
-#include<iostream>
 #include<fstream>
 #include<stdlib.h>
+#include<string>
+#include<iostream>
 #include<string.h>
+
+using std::string;
+using std::ifstream;
 
 class Reader
 {
 	private:
-		char *filename;
-		char *path = "../files/";
+		string filename;
+		static const string path;
+		int errortype;
+		void error(int type);
+		string read_file(ifstream *s);
+		int done_check(char *s);
 	public:
-		Reader (const char *file);
-		~Reader ();
-		char *all ();
-		char *get_filename();
+		Reader(string file);
+		~Reader();
+		int get_error();
+		string get_filename();
+		string all();
+		string done();
+		string undone();
 };
 
 #endif
--- src/reader.cc
+++ src/reader.cc
@@ -1,41 +1,121 @@
 #include"reader.h"
-using namespace std;
+#include<stdio.h>
 
-Reader::Reader(const char *file)
+const string Reader::path = "../files/";
+
+void Reader::error(int type)
+{
+	errortype = type;
+}
+
+Reader::Reader(string file)
 {
 	int n, i;
+	const char *it;
 	n = 0;
 	i = 0;
-	while (file[i] != '\0') {
-		if (file[i] == '.') {
+	it = file.data();
+	while (it[i] != '\0') {
+		if (it[i] == '.') {
 			n++;
 		} else {
 			n = 0;
 		}
 		if (n == 2) {
-			filename = (char *)calloc(1, sizeof(char));
-			*filename = '\0';
+			error(1);
 			return;
 		}
 		i++;
 	}
-	int length;
-	length = strlen(file);
-	filename = (char *)calloc(length, sizeof(char));
-	strcpy(filename, file);
+	if (it[i-1] == '/') {
+		error(1);
+		return;
+	}
+	errortype = 0;
+	filename = it;
 }
 
 Reader::~Reader()
 {
-	free(filename);
 }
 
-char *Reader::all()
+int Reader::get_error()
 {
-	fstream s;
-	s.open(filename);
-	char *ret;
-	ret = (char *)malloc(1);
-	*ret = '\0';
-	return ret;
+	return errortype;
+}
+
+string Reader::get_filename()
+{
+	return filename;
+}
+
+int Reader::done_check(char *s)
+{
+	int i, length;
+	char done[] = "(done) ";
+	char *temp;
+	length = strlen(s);
+
+	i = 0;
+	while (s[i] == '\t' && s[i] != '\0') {
+		i++;
+	}
+
+	if (s[i] != '!') {
+		return 0;
+	}
+
+	temp = (char *)malloc((length - i + 1) * sizeof(char));
+	strncpy(temp, s + i + 1, length - i + 1);
+	strncpy(s + i, done, 7);
+	memcpy(s + i + 7, temp, length - i + 1);
+	free(temp);
+	return 1;
+}
+
+
+string Reader::read_file(ifstream *s)
+{
+	string *out;
+	char *current;
+	out = new string();
+	current = (char *)calloc(100, sizeof(char));
+
+	while (!s->eof() && out->size() + 100 < out->max_size()) {
+		s->getline(current, 100);
+		done_check(current);
+		*out += current;
+		*out += '\n';
+	}
+
+	while (out->back() == '\n') {
+		out->pop_back();
+	}
+	return *out;
+}
+
+string Reader::all()
+{
+	string out;
+	ifstream *s;
+	s = new ifstream();
+	s->open((path + filename).c_str());
+	if (s->is_open()) {
+		out = read_file(s);
+	}
+	s->close();
+	s->~ifstream();
+	return out;
+}
+
+string Reader::done()
+{
+	string out;
+	return out;
+}
+
+string Reader::undone()
+{
+	string out;
+	return out;
 }
